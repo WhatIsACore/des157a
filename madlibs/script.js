df8911f8574f38c054f2b2dc44bb0ec2ffd71506
zeki <zekixu@campus-024-070.ucdavis.edu>
2023-10-18 14:05:35 -0700

    test

---
+++ madlibs/script.js
@@ -0,0 +1,38 @@
+let name = '';
+let animal = '';
+let verbing = '';
+let place = '';
+let verb = '';
+let noun = '';
+let adj = '';
+let verbing2 = '';
+let adj2 = '';
+let adj3 = '';
+let nouns = '';
+let adj4 = '';
+let friend = '';
+let adj5 = '';
+let verbing3 = '';
+let n = '';
+let verb2 = '';
+let nouns2 = '';
+let nouns3 = '';
+let verb3 = '';
+let n2 = '';
+let adj6 = '';
+let adj7 = '';
+let adj8 = '';
+
+
+res = `
+    ${name} the ${animal}
+
+    One night, while ${name} was ${verbing} in the ${place}, they suddenly ${verb} upon a ${noun} that looked particularly ${adj}. ${name} couldn't resist the temptation and pounced on it, ${verbing2} with delight.
+    To their surprise, the ${noun} turned out to be a ${adj2} portal that transported ${name} to a ${adj3} world filled with ${nouns}. Their adventure had just begun!
+
+    In this ${adj3} land, ${name} met a ${adj4} ${friend} who spoke in ${adj5} rhymes. They began ${verbing3} together, embarking on ${n} quests together to ${verb2} the land.
+    As they journeyed through this ${adj3} realm, ${name} discovered their true goal, which was to bring ${nouns2} and ${nouns3} to everyone they met. With their newfound ${friend}, they went on to ${verb3} everywhere.
+
+    Finally, after ${n2} days, ${name} found another magical portal that led them back to the ${place}. They returned home, from their ${adj6} adventure, but forever changed.
+    From that day on, ${name} was ${adj7} and ${adj8}, never forgetting the adventure.
+`;
